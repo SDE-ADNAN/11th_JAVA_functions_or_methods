3cc1c0678bfc63287c1babb36ba28dbb51771ddd
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-03-17 23:49:18 +0530

    made a function returning string value

---
+++ src/com/adnan/Function_returning_string_value.java
@@ -0,0 +1,13 @@
+package com.adnan;
+
+public class Function_returning_string_value {
+    public static void main(String[] args) {
+        String message = greet();
+        System.out.println(message);
+    }
+
+    static String greet(){
+        String greet = "hello how are  u  ....";
+        return greet;
+    }
+}
