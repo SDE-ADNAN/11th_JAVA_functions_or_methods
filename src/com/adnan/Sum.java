9d7e47ef38ee229b3cb998a4fdf3b9a1865dd326
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-03-17 19:40:20 +0530

    made the sum function to return the sum variable

--- src/com/adnan/Sum.java
+++ src/com/adnan/Sum.java
@@ -5,7 +5,22 @@ import java.util.Scanner;
 public class Sum {
 
     public static void main(String[] args) {
-	sum();
+//	sum();
+        int ans = sum2();
+        System.out.println(ans);
+    }
+
+    //function returning a value
+    static int sum2(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter first number  : ");
+        int num1 = in.nextInt();
+        System.out.println("Enter second number : ");
+        int num2 = in.nextInt();
+        System.out.println("sum is : ");
+        int sum = num1+num2;
+        return sum;
+
     }
 
     static void sum (){
