b900194b1cdd1c956f6dd4b959404bcc5b15f47b
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-03-18 11:17:54 +0530

    made a changevalue class to change value of the given array at index 0

--- src/com/adnan/Swap.java
+++ src/com/adnan/Swap.java
@@ -8,12 +8,18 @@ public class Swap {
         int a = 10;
         int b = 20;
 
+        // Swapping two numbers
+//        int temp =a;
+//        a=b;
+//        b = temp;
+        //
+
+        System.out.println(a+" "+b);
+    }
+    static void swap(int a,int b){
         int temp =a;
         a=b;
         b = temp;
-        System.out.println(a+" "+b);
-
-
-
     }
+
 }
