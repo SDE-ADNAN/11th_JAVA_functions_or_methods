82927451074ecef294ec65dd1281654186854f66
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-03-18 16:19:33 +0530

    learned about block scope

--- src/com/adnan/Scope.java
+++ src/com/adnan/Scope.java
@@ -5,6 +5,13 @@ public class Scope {
         int a=10;
         int b = 20;
 //        System.out.println(num); // not available here as it is defined in another function
+
+        { // block start
+            a= 20; // can change any variable's value  but cannot initialize a variable with same variable name
+            int as = 33;
+        } //block end
+        System.out.println(a);
+//        System.out.println(as);  // as variable is not available here due to restrictions of block scope
     }
 
     static void random(){
